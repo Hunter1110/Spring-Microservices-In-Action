09b387f232fc76a78bbb9d83b5ed1c135be7507a
Hunter1110 <wenfeng_zhang@aopen.com>
2018-11-07 16:57:51 +0800

    licensing-service调用organization-service获取Organization 两种调用方式: Discovery和Rest

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -2,11 +2,21 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class Application {
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
